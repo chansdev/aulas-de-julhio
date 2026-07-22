2ceabeaf48d919a62fbb4afb697e09abb599c85d
Heitor Chans <164504718+chansdev@users.noreply.github.com>
2026-07-22 12:09:36 +0000

    dnv

--- react-julio/src/App.jsx
+++ react-julio/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import BemVindo from "./pages/BemVindo";
+import Home from "../components/Home";
+import BemVindo from "../components/BemVindo";
 
 function App() {
   return (
